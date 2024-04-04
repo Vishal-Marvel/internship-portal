7d1db30690e39710c2739abc4bf95eeaf26d108f
T-cube2512 <srivenk03@gmail.com>
2024-04-04 12:06:51 +0530

    carousel added

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div className="grid place-items-center w-full">
+  return (
+  <div className="grid place-items-center w-full">
     
-  </div>;
+  </div>);
 };
 
 export default Dashboard;
