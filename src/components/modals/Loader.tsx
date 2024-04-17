108b2cd319d14716e0bef60a7b1a791457eeca3c
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2024-04-17 10:10:03 +0530

    backdrop blur added for loader

--- src/components/modals/Loader.tsx
+++ src/components/modals/Loader.tsx
@@ -11,12 +11,12 @@ export const LoaderModal = () => {
   return (
     <div
       className={cn(
-        " fixed top-0 left-0 w-full h-full backdrop-brightness-50 justify-center align-middle items-center z-50",
+        " fixed top-0 left-0 w-full h-full backdrop-brightness-50 backdrop-blur-sm justify-center align-middle items-center z-50",
         isModalOpen ? "block" : "hidden"
       )}
     >
       <div className="absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="relative bg-slate-50/50 rounded-full">
+        <div className="relative">
           <img
             src="/logo.png"
             className="aspect-auto h-10 absolute z-[100] top-16 left-10"
